b1c638001550ca22412c7fe2669a2c50c5d3460e
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2024-08-04 11:19:19 +0200

    Script

--- installer/linux/deb/worker/usr/share/worker/script.sh
+++ installer/linux/deb/worker/usr/share/worker/script.sh
@@ -1,1 +1,11 @@
-mkdir "/prova"
+#!/bin/bash
+
+if ! command -v podman &> /dev/null
+then
+    echo "Podman non è installato. Installazione in corso..."
+
+    sudo apt-get update
+    sudo apt-get install -y podman
+fi
+
+podman run --rm alessandrotalmi/worker
