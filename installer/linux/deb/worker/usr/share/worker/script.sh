921b28bba3c9a294347bb796be90e203228b38c7
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2024-08-04 12:20:50 +0200

    installer

--- installer/linux/deb/worker/usr/share/worker/script.sh
+++ installer/linux/deb/worker/usr/share/worker/script.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-if ! command -v podman &> /dev/null
-then
-    echo "Podman non è installato. Installazione in corso..."
+command_exists() {
+    command -v "$1" &> /dev/null
+}
+
+if ! command_exists podman; then
+    echo "Installing podman..."
 
     sudo apt-get update
     sudo apt-get install -y podman
 fi
 
-podman run --rm alessandrotalmi/worker
+IMAGE_NAME="docker.io/alessandrotalmi/worker:latest"
+CONTAINER_NAME="worker"
+
+echo "Retrieving latest docker image..."
+podman pull $IMAGE_NAME
+
+if podman ps -a --format '{{.Names}}' | grep -Eq "^${CONTAINER_NAME}$"; then
+    echo "Removing existing container named ${CONTAINER_NAME}..."
+    podman rm -f $CONTAINER_NAME
+fi
+
+echo "Executing the container..."
+podman run --rm --name $CONTAINER_NAME $IMAGE_NAME
+
