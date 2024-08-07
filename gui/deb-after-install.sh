b044355ebd41a84efc9cd5fe133d4d02a490dea3
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2024-08-07 20:46:29 +0200

    Da electron

--- gui/deb-after-install.sh
+++
@@ -1,4 +0,0 @@
-IMAGE_NAME="docker.io/alessandrotalmi/worker:latest"
-
-echo "Retrieving latest docker image..."
-podman pull $IMAGE_NAME
--- gui/main.js
+++ gui/main.js
@@ -19,6 +19,32 @@ function createWindow() {
     mainWindow.on('closed', function () {
         mainWindow = null;
     });
+
+    // Esegui il comando per aggiornare i pacchetti
+    exec('sudo apt update', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Errore nell'aggiornamento dei pacchetti: ${error.message}`);
+            dialog.showErrorBox('Errore', `Errore nell'aggiornamento dei pacchetti: ${error.message}`);
+            return;
+        }
+
+        console.log(`Output aggiornamento pacchetti:\n${stdout}`);
+
+        // Esegui il comando per installare Podman
+        exec('sudo apt install podman -y', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Errore nell'installazione di Podman: ${error.message}`);
+                dialog.showErrorBox('Errore', `Errore nell'installazione di Podman: ${error.message}`);
+                return;
+            }
+
+            console.log(`Output installazione Podman:\n${stdout}`);
+            dialog.showMessageBox({
+                type: 'info',
+                message: 'Podman è stato installato con successo!',
+            });
+        });
+    });
 }
 
 function getUrl(){
