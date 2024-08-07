387b14526c62d7048bc6fd5d921662ab37d913cb
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2024-08-07 09:55:19 +0200

    Installer by electron

---
+++ gui/deb-after-install.sh
@@ -0,0 +1,4 @@
+IMAGE_NAME="docker.io/alessandrotalmi/worker:latest"
+
+echo "Retrieving latest docker image..."
+podman pull $IMAGE_NAME
--- installer/linux/arm64/deb/create-installer.sh
+++
@@ -1,3 +0,0 @@
-mv ../../../../gui/release/WorkerGui-arm64.deb ./worker/WorkerGui-arm64.deb
-chmod +x ./worker/WorkerGui-arm64.deb
-dpkg-deb --build worker
